6a09fc26a0bd28345f831b478a047d9efa73b60e
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2014-09-30 15:32:37 +0800

    using localhost mysql BD

--- api/models/User.js
+++ api/models/User.js
@@ -1,14 +1,18 @@
 /**
-* User.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
 
 module.exports = {
-
-  attributes: {
-
-  }
+    connection: "localMysqlServer",
+    attributes: {
+        name: {
+            type: "String"
+        },login:{
+            type: "String"
+        }
+    }
 };
 
