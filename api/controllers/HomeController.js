a490e40f76299c9e4f1165fc29d5bc1820b430b1
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2014-10-01 18:26:14 +0800

    save all changes

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -8,7 +8,7 @@
 module.exports = {
     index: function (req, res) {
         res.view(null, {
-            title: 'Home'
+            title: 'Главная'
         });
     }
 };
---
+++ api/policies/currentUser.js
@@ -0,0 +1,17 @@
+/**
+ * Gets the current user from session, or returns 403
+ */
+module.exports = function (req, res, next) {
+    console.log(req.body);
+    console.log(req.session);
+    // User is allowed, proceed to controller
+    if (req.session.user) {
+        req.body.user = req.session.user;
+        return next();
+    }
+
+    // User is not allowed
+    else {
+        return res.forbidden('You are not permitted to perform this action.');//NOTE: old version: send("You are not permitted to perform this action.", 403);
+    }
+};
